e29194706cb87bd50fb9e1a2d14a30b42f68529a
lucianoperrella <lucianoperrella@gmail.com>
2024-08-14 13:51:05 -0300

    Iniciando repositorio

---
+++ 1.py
@@ -0,0 +1,3 @@
+'''Crear una función que retorne una lista que tenga los valores pares que se
+encuentren dentro de un rango dado (dicho rango se recibe como parámetro
+de la función: desde/hasta)'''
