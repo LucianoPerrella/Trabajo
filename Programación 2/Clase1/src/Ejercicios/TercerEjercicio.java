ab34d1e9259f734f2aacf1d974c5e229c36a9a2c
lucianoperrella <lucianoperrella@gmail.com>
2025-03-24 14:18:52 -0300

    Primeros pasos

---
+++ Programación 2/Clase1/src/Ejercicios/TercerEjercicio.java
@@ -0,0 +1,35 @@
+package Ejercicios;
+
+public class TercerEjercicio {
+	/*
+	 EJERCICIO 3. Escriba un método en Java que:
+	a. Reciba un número y determine si es par.
+	
+	b. Reciba un número y determine si es múltiplo de 5 o 7.
+	
+	c. Reciba dos números y retorne el mayor de ellos.
+	
+	d. Reciba tres números y retorne el mayor de los tres.
+	
+	e. Reciba un número entre 0 y 10, y si el número está entre 0 y 4 muestre “desaprobado”, si está entre 5 y 6
+	muestre “suspenso”, y si es mayor que 6 muestre “aprobado”.
+	
+	f. Muestre por pantalla todos los números entre 0 y 100.
+	
+	g. Reciba un número N, y muestre por pantalla todos los enteros entre 0 y N
+	que sean pares.
+	
+	h. Reciba un número N, y retorne la sumatoria desde 0 a N.
+	
+	i. Reciba un número N, y retorne la suma de sus dígitos
+	
+	j. Determine si, dado un número N = dm,dm-1, ..., d1 y un dígito d, d=di para
+	1<= i <= m
+	
+	k. Determine si un número dado es primo.
+	
+	l. Determine si un número dado es perfecto; un número es perfecto si la suma de los divisores del número
+	(incluyendo al 1 y excluyendo al número) es igual al número.
+	Ej.: suma de divisores de 6 =>1 + 2 + 3 = 6 => es perfecto*/
+
+}
---
+++ Programación 2/Clase1/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1; int b=2; 
+		Boolean v=!(a==1 && ++b==1);
+		System.out.println(v);
+		
+		
+		
+	}
+
+}
