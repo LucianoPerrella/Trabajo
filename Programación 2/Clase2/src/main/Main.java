15f682b623d8c1c8f5e6e24e8cb4d0d056087181
lucianoperrella <lucianoperrella@gmail.com>
2025-03-24 22:00:28 -0300

    Avanzando con Progra 2

---
+++ Programación 2/Clase2/src/main/Main.java
@@ -0,0 +1,46 @@
+package main;
+
+import primerEjercicio.CajaDeAhorro;
+import segundoEjercicio.Flor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		--------------Ejercicio 1--------------
+//		CajaDeAhorro primerCaja = new CajaDeAhorro(20);
+//		CajaDeAhorro segundaCaja = new CajaDeAhorro();
+//		
+//		
+//		System.out.println(primerCaja.getNumCaja());
+//		System.out.println(segundaCaja.getNumCaja());
+//		
+//		System.out.println(primerCaja.getSaldo());
+//		
+//		primerCaja.setDeposito(20);
+//		System.out.println(primerCaja.getSaldo());
+//		
+//		primerCaja.setExtraccion(50);
+//		System.out.println(primerCaja.getSaldo());
+		
+		
+//		--------------Ejercicio 2--------------
+		
+//		Flor primerFlor = new Flor();
+//		System.out.println(primerFlor.getNombre());
+//		System.out.println(primerFlor.getNumPetalos());
+//		System.out.println(primerFlor.getPrecio());
+//		
+//		primerFlor.setNombre("Petunia");
+//		primerFlor.setPrecio(50.0f);
+//		primerFlor.setNumPetalos(25);
+//		System.out.println(primerFlor.getNombre());
+//		System.out.println(primerFlor.getNumPetalos());
+//		System.out.println(primerFlor.getPrecio());
+
+		
+//		--------------Ejercicio 3--------------
+	}
+
+}
---
+++ Programación 2/Clase2/src/tercerEjercicio/Equipo.java
@@ -0,0 +1,26 @@
+package tercerEjercicio;
+
+public class Equipo {
+	
+//	En un campeonato de fútbol por cada partido ganado se obtienen 3 puntos y por cada
+//	empate se logra 1. Cada equipo tiene un nombre, un capitán, una cantidad de partidos
+//	ganados, otra de empatados y otra de perdidos, una cantidad de goles a favor y otra de
+//	goles en contra. El capitán es un jugador que tiene un nombre, un año de nacimiento,
+//	un número de camiseta, un número que representa la posición en la que juega, la
+//	cantidad de partidos jugados y la cantidad de goles convertidos en el campeonato.
+//	Para un jugador se desea calcular el promedio de goles de un jugador por partido y dado
+//	otro jugador, cuál es el que hizo más goles. Para un equipo se desea calcular los partidos
+//	jugados y los puntos obtenidos. Además, para otro equipo dado es necesario decidir cuál
+//	es el equipo con mayor puntaje y cuál es el capitán con más goles. Si dos equipos tienen
+//	los mismos puntos, se devuelve el que tiene mayor cantidad de goles a favor y si también
+//	hay coincidencia se consideran los goles en contra. Si hay coincidencia se devuelve uno
+//	cualquiera. 
+//	
+//	1. A partir del diagrama de clases en UML y la especificación de requerimientos
+//	implemente dos clases en Java encapsulando atributos y comportamiento para
+//	modelar la situación planteada. Incluya dentro del comportamiento los
+//	comandos para modificar cada atributo y las consultas para devolver cada
+//	atributo.
+//	2. Elabore una clase que permita testear el modelo.
+
+}
